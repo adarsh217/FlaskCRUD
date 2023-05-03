bfbda2ba1cafe5a30e8468093e64e8758309d836
Adarsh Rai <59870032+adarsh217@users.noreply.github.com>
2023-05-04 03:12:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,65 +10,48 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['db']
 collection = db['users']
 
-class User(Resource):
-    def get(self, user_id=None):
-        if user_id:
-            user = collection.find_one({"_id": ObjectId(user_id)})
-            if user:
-                user['_id'] = str(user['_id'])
-                return {"message": "User found", "data": user}, 200
-            else:
-                return {"message": "User not found"}, 404
-        else:
-            users = []
-            for user in collection.find():
-                user['_id'] = str(user['_id'])
-                users.append(user)
-            return {"message": "Users found", "data": users}, 200
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str)
+parser.add_argument('email', type=str)
+parser.add_argument('password', type=str)
+
+class Users(Resource):
+    def get(self):
+        users = db.users.find()
+        return list(users)
 
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str, required=True)
-        parser.add_argument('email', type=str, required=True)
-        parser.add_argument('password', type=str, required=True)
         args = parser.parse_args()
+        user_id = db.users.insert_one(args).inserted_id
+        return {'id': str(user_id)}
 
-        user = {"name": args['name'], "email": args['email'], "password": args['password']}
-        result = collection.insert_one(user)
-
-        user['_id'] = str(result.inserted_id)
-        return {"message": "User created", "data": user}, 201
+class User(Resource):
+    def get(self, user_id):
+        user = db.users.find_one({'_id': ObjectId(user_id)})
+        if user is not None:
+            return user
+        else:
+            return {'error': 'User not found'}
 
     def put(self, user_id):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str)
-        parser.add_argument('email', type=str)
-        parser.add_argument('password', type=str)
         args = parser.parse_args()
-
-        user = collection.find_one({"_id": ObjectId(user_id)})
-        if user:
-            updated_user = {}
-            for field in ['name', 'email', 'password']:
-                if args[field]:
-                    updated_user[field] = args[field]
-                else:
-                    updated_user[field] = user[field]
-
-            collection.update_one({"_id": ObjectId(user_id)}, {"$set": updated_user})
-            updated_user['_id'] = str(user_id)
-            return {"message": "User updated", "data": updated_user}, 200
-        else:
-            return {"message": "User not found"}, 404
+        db.users.update_one({'_id': ObjectId(user_id)}, {'$set': args})
+        return {'id': user_id}
 
     def delete(self, user_id):
-        result = collection.delete_one({"_id": ObjectId(user_id)})
-        if result.deleted_count:
-            return {"message": "User deleted"}, 204
-        else:
-            return {"message": "User not found"}, 404
+        db.users.delete_one({'_id': ObjectId(user_id)})
+        return {'id': user_id
+                
+class UserTomorrow(Resource):
+    def get(self, user_id, tomorrow):
+        
 
-api.add_resource(User, '/users', '/users/<string:user_id>')
+api.add_resource(Users, '/users')
+api.add_resource(User, '/users/<user_id>')
+api.add_resource(UserTomorrow, '/users/<user_id>/<tomorrow>')
+
+api.add_resource(Users, '/users')
+api.add_resource(User, '/users/<user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
